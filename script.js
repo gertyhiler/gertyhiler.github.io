eaafdb61847497d4934a1245b2049cd0a341f08c
gertyhiler <36032455+gertyhiler@users.noreply.github.com>
2019-12-26 23:41:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//let text = document.getElementById('text').addEventListener('click', clear);
+let fun ={
+    clear(){
+        document.getElementById("text").value = ""
+        alert("Грехи отпущены!")
+    }
+}
+document.addEventListener("keypress", doc=()=> event.key == "Enter"?fun.clear(): false)
